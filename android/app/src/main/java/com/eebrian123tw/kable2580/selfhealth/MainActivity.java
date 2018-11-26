fd8d30583fbeff3ee18539edfca4919e33eaf96d
brianlu <eebrian123tw93@gmail.com>
2018-11-26 22:58:35 +0800

    main activity layout update

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/MainActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/MainActivity.java
@@ -1,14 +1,20 @@
 package com.eebrian123tw.kable2580.selfhealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button shareButton;
     private Button exportImportButton;
+
+    private Button stepButton;
+    private Button sleepButton;
+    private Button drinkButton;
+    private Button usePhoneButton;
 
 
     @Override
@@ -19,25 +25,47 @@ public class MainActivity extends AppCompatActivity {
 
         shareButton = findViewById(R.id.share_button);
         exportImportButton = findViewById(R.id.export_import_button);
+        stepButton = findViewById(R.id.step_button);
+        sleepButton = findViewById(R.id.sleep_button);
+        drinkButton = findViewById(R.id.drink_button);
+        usePhoneButton = findViewById(R.id.use_phone_button);
 
-
-        shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
-        exportImportButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        shareButton.setOnClickListener(this);
+        exportImportButton.setOnClickListener(this);
+        stepButton.setOnClickListener(this);
+        sleepButton.setOnClickListener(this);
+        drinkButton.setOnClickListener(this);
+        usePhoneButton.setOnClickListener(this);
 
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.step_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.sleep_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.drink_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.use_phone_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.share_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+            case R.id.export_import_button:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+
+
+    }
 }
