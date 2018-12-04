8dfe9f86c10460e374fd4944b7b241474fd60634
CONWAY <kable2580@gmail.com>
2018-12-05 06:03:58 +0800

    settings activity

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/SettingsActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/SettingsActivity.java
@@ -2,12 +2,27 @@ package com.eebrian123tw.kable2580.selfhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+  private Switch showNotificationSwitch;
+  private Switch showStepsSwitch;
+  private Switch showSleepDataSwitch;
+  private Switch showWaterAmountSwitch;
+  private Switch showPhoneUsageSwtich;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
+
+    showNotificationSwitch = findViewById(R.id.show_notifications_switch);
+    showStepsSwitch = findViewById(R.id.show_steps_switch);
+    showSleepDataSwitch = findViewById(R.id.show_sleep_data_switch);
+    showWaterAmountSwitch = findViewById(R.id.show_water_amount_switch);
+    showPhoneUsageSwtich = findViewById(R.id.show_phone_usage_switch);
+
+
   }
 }
