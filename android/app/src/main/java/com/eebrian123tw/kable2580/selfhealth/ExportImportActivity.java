babdd73af2faf230c099278b00567ae5bb303a3b
brianlu <eebrian123tw93@gmail.com>
2018-12-07 21:39:19 +0800

    export import filename fix

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
@@ -1,14 +1,20 @@
 package com.eebrian123tw.kable2580.selfhealth;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,15 +25,15 @@ import com.obsez.android.lib.filechooser.ChooserDialog;
 import java.io.File;
 import java.util.regex.Pattern;
 
-public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener {
+public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener,View.OnTouchListener {
 
     private Button exportButton;
     private Button importButton;
     private TextView
             fileNameTextView; // https://stackoverflow.com/questions/7856959/android-file-chooser
-    private static final int FILE_SELECT_CODE = 1000;
     private static final String TAG = "ExportImportActivity";
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +46,9 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
         importButton.setOnClickListener(this);
 
         fileNameTextView = findViewById(R.id.file_name_textview);
+
+        fileNameTextView.setOnTouchListener(this);
+
     }
 
     @Override
@@ -50,10 +59,11 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
                         .withFilter(true, false)
                         .withStartFile(Environment.getExternalStorageDirectory().getPath() + "/")
                         .withChosenListener(new ChooserDialog.Result() {
+                            @SuppressLint("SetTextI18n")
                             @Override
                             public void onChoosePath(String path, File pathFile) {
                                 Toast.makeText(ExportImportActivity.this, "FOLDER: " + path, Toast.LENGTH_SHORT).show();
-                                fileNameTextView.setText(getString(R.string.file_name_string) + path+"/selfHealth.json");
+                                fileNameTextView.setText(getString(R.string.file_name_string)+": " + path+"/selfHealth.json");
                             }
                         })
                         .build()
@@ -65,10 +75,11 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
                         .withFilter(false, false, "json")
                         .withStartFile(Environment.getExternalStorageDirectory().getPath() + "/")
                         .withChosenListener(new ChooserDialog.Result() {
+                            @SuppressLint("SetTextI18n")
                             @Override
                             public void onChoosePath(String s, File file) {
                                 Toast.makeText(ExportImportActivity.this, "FILE: " + file, Toast.LENGTH_SHORT).show();
-                                fileNameTextView.setText(getString(R.string.file_name_string) + file);
+                                fileNameTextView.setText(getString(R.string.file_name_string)+": " + file);
                             }
                         })
                         .build()
@@ -79,4 +90,21 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
 
 
 
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (v.getId()){
+            case R.id.file_name_textview:
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    fileNameTextView.setLines(0);
+                    fileNameTextView.setEllipsize(null);
+                }else if(event.getAction()==MotionEvent.ACTION_UP) {
+                    fileNameTextView.setLines(1);
+                    fileNameTextView.setEllipsize(TextUtils.TruncateAt.END);
+                }
+                break;
+        }
+        return true;
+    }
 }
