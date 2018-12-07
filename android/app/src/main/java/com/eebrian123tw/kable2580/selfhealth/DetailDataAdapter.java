3e68785fcefe3b39f30dd526ef45be11e6ff1cce
brianlu <eebrian123tw93@gmail.com>
2018-12-07 13:29:41 +0800

    DetailDataUnit fix
    DetailDataUnit

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataAdapter.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataAdapter.java
@@ -8,48 +8,70 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.threeten.bp.LocalDate;
+
 import java.util.List;
 
 public class DetailDataAdapter extends RecyclerView.Adapter<DetailDataAdapter.ViewHolder> {
-  private List<DetailDataUnit> detailData;
-  private Context context;
+    private List<DetailDataUnit> detailData;
+    private Context context;
 
-  public DetailDataAdapter(Context context, List<DetailDataUnit> detailData) {
-    this.context = context;
-    this.detailData = detailData;
-  }
-
-  @NonNull
-  @Override
-  public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-    View view =
-        LayoutInflater.from(context).inflate(R.layout.detail_data_unit_cardview, viewGroup, false);
-    return new ViewHolder(view);
-  }
-
-  @Override
-  public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-    DetailDataUnit detailDataUnit = detailData.get(i);
-    viewHolder.dataTimeTextView.setText(detailDataUnit.getDataTime());
-    viewHolder.dataTypeTextView.setText(detailDataUnit.getType());
-    viewHolder.valueTextView.setText(String.valueOf(detailDataUnit.getValue()));
-  }
-
-  @Override
-  public int getItemCount() {
-    return detailData.size();
-  }
-
-  class ViewHolder extends RecyclerView.ViewHolder {
-    private TextView dataTimeTextView;
-    private TextView dataTypeTextView;
-    private TextView valueTextView;
-
-    public ViewHolder(@NonNull View itemView) {
-      super(itemView);
-      dataTimeTextView = itemView.findViewById(R.id.datatime_textview);
-      dataTypeTextView = itemView.findViewById(R.id.data_type_textview);
-      valueTextView = itemView.findViewById(R.id.value_textview);
+    public DetailDataAdapter(Context context, List<DetailDataUnit> detailData) {
+        this.context = context;
+        this.detailData = detailData;
     }
-  }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view =
+                LayoutInflater.from(context).inflate(R.layout.detail_data_unit_cardview, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        DetailDataUnit detailDataUnit = detailData.get(i);
+        LocalDate localDate = detailDataUnit.getDataTime();
+        DetailDataUnit.Type type = detailDataUnit.getType();
+        String stringType;
+        switch (type) {
+            case STEPS:
+                stringType = context.getString(R.string.step);
+                break;
+            case DRINK:
+                stringType = context.getString(R.string.drink);
+                break;
+            case SLEEP:
+                stringType = context.getString(R.string.sleep);
+                break;
+            case PHONE_USE:
+                stringType = context.getString(R.string.phone_use);
+                break;
+            default:
+                stringType = context.getString(R.string.step);
+
+        }
+        viewHolder.dataTimeTextView.setText(localDate.toString());
+        viewHolder.dataTypeTextView.setText(stringType);
+        viewHolder.valueTextView.setText(String.valueOf(detailDataUnit.getValue()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return detailData.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView dataTimeTextView;
+        private TextView dataTypeTextView;
+        private TextView valueTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dataTimeTextView = itemView.findViewById(R.id.datatime_textview);
+            dataTypeTextView = itemView.findViewById(R.id.data_type_textview);
+            valueTextView = itemView.findViewById(R.id.value_textview);
+        }
+    }
 }
--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataUnit.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/DetailDataUnit.java
@@ -1,16 +1,22 @@
 package com.eebrian123tw.kable2580.selfhealth;
 
+import org.threeten.bp.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class DetailDataUnit {
-  private String type;
-  private double value;
-  private String dataTime;
+    private Type type;
+    private double value;
+    private LocalDate dataTime;
 
-  public DetailDataUnit(String type, double value, String dataTime) {
-    this.type = type;
-    this.value = value;
-    this.dataTime = dataTime;
-  }
+    public DetailDataUnit(Type type, double value, LocalDate dataTime) {
+        this.type = type;
+        this.value = value;
+        this.dataTime = dataTime;
+    }
+
+    public enum Type {
+        SLEEP, STEPS, PHONE_USE, DRINK
+    }
 }
