b84747b93f8933e0351fdf464869a011e8137229
brianlu <eebrian123tw93@gmail.com>
2018-12-07 14:12:26 +0800

    choose  file  function

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
@@ -1,39 +1,67 @@
 package com.eebrian123tw.kable2580.selfhealth;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener {
 
-  private Button exportButton;
-  private Button importButton;
-  private TextView
-      fileNameTextView; // https://stackoverflow.com/questions/7856959/android-file-chooser
+import com.obsez.android.lib.filechooser.ChooserDialog;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_export_import);
+import java.io.File;
+import java.util.regex.Pattern;
 
-    exportButton = findViewById(R.id.export_button);
-    importButton = findViewById(R.id.import_button);
+public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener,ChooserDialog.Result {
 
-    exportButton.setOnClickListener(this);
-    importButton.setOnClickListener(this);
+    private Button exportButton;
+    private Button importButton;
+    private TextView
+            fileNameTextView; // https://stackoverflow.com/questions/7856959/android-file-chooser
+    private static final int FILE_SELECT_CODE = 1000;
+    private static final String TAG = "ExportImportActivity";
 
-    fileNameTextView = findViewById(R.id.file_name_textview);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export_import);
 
-  @Override
-  public void onClick(View v) {
-    switch (v.getId()) {
-      case R.id.export_button:
-        break;
-      case R.id.import_button:
-        break;
+        exportButton = findViewById(R.id.export_button);
+        importButton = findViewById(R.id.import_button);
+
+        exportButton.setOnClickListener(this);
+        importButton.setOnClickListener(this);
+
+        fileNameTextView = findViewById(R.id.file_name_textview);
     }
-  }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.export_button:
+                break;
+            case R.id.import_button:
+                new ChooserDialog(this)
+//                        .withFilter(false, false, "json")
+                        .withStartFile(Environment.getExternalStorageDirectory().getPath() + "/")
+                        .withChosenListener(this)
+                        .build()
+                        .show();
+                break;
+        }
+    }
+
+
+    @Override
+    public void onChoosePath(String s, File file) {
+        Toast.makeText(ExportImportActivity.this, "FILE: " + file, Toast.LENGTH_SHORT).show();
+        fileNameTextView.setText("FILE: " + file);
+    }
 }
