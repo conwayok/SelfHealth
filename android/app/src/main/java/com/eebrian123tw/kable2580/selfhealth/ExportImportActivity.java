0df66d540448e3bc9f7483ff0cbed42ad4578af0
brianlu <eebrian123tw93@gmail.com>
2018-12-08 23:50:08 +0800

    import data

--- android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/ExportImportActivity.java
@@ -1,31 +1,37 @@
 package com.eebrian123tw.kable2580.selfhealth;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.eebrian123tw.kable2580.selfhealth.dao.HealthDataDao;
+import com.eebrian123tw.kable2580.selfhealth.service.entity.DailyDataModel;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.obsez.android.lib.filechooser.ChooserDialog;
 
-import java.io.File;
-import java.util.regex.Pattern;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Month;
 
-public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener,View.OnTouchListener {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ExportImportActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
     private Button exportButton;
     private Button importButton;
@@ -51,6 +57,26 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
 
     }
 
+    public static class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+        public LocalDateDeserializer() {
+            super(LocalDate.class);
+        }
+
+        @Override
+        public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt)
+                throws IOException {
+            try {
+                JsonNode node = jp.getCodec().readTree(jp);
+                int year = Integer.parseInt(node.get("year").asText());
+                Month month = Month.valueOf(node.get("month").asText());
+                int dayOfMonth = Integer.parseInt(node.get("dayOfMonth").asText());
+                return LocalDate.of(year, month, dayOfMonth);
+            } catch (final Exception e) {
+                throw new IOException(e);
+            }
+        }
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -63,7 +89,7 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
                             @Override
                             public void onChoosePath(String path, File pathFile) {
                                 Toast.makeText(ExportImportActivity.this, "FOLDER: " + path, Toast.LENGTH_SHORT).show();
-                                fileNameTextView.setText(getString(R.string.file_name_string)+": " + path+"/selfHealth.json");
+                                fileNameTextView.setText(getString(R.string.file_name_string) + ": " + path + "/selfHealth.json");
                             }
                         })
                         .build()
@@ -79,7 +105,8 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
                             @Override
                             public void onChoosePath(String s, File file) {
                                 Toast.makeText(ExportImportActivity.this, "FILE: " + file, Toast.LENGTH_SHORT).show();
-                                fileNameTextView.setText(getString(R.string.file_name_string)+": " + file);
+                                fileNameTextView.setText(getString(R.string.file_name_string) + ": " + file);
+                                importData(file);
                             }
                         })
                         .build()
@@ -89,22 +116,39 @@ public class ExportImportActivity extends AppCompatActivity implements View.OnCl
     }
 
 
-
-
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.file_name_textview:
-                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     fileNameTextView.setLines(0);
                     fileNameTextView.setEllipsize(null);
-                }else if(event.getAction()==MotionEvent.ACTION_UP) {
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     fileNameTextView.setLines(1);
                     fileNameTextView.setEllipsize(TextUtils.TruncateAt.END);
                 }
                 break;
         }
         return true;
+    }
+
+    private void importData(File file) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeFactory typeFactory = mapper.getTypeFactory();
+            SimpleModule module = new SimpleModule();
+            module.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+            mapper.registerModule(module);
+            mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+            List<DailyDataModel> dailyDataModelList = mapper.readValue(file, typeFactory.constructCollectionType(List.class, DailyDataModel.class));
+            Log.i(TAG, dailyDataModelList.size() + "");
+            Toast.makeText(ExportImportActivity.this, "import " + dailyDataModelList.size() + " counts", Toast.LENGTH_SHORT).show();
+            HealthDataDao healthDataDao = new HealthDataDao(ExportImportActivity.this);
+            healthDataDao.saveDailyData(dailyDataModelList);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(ExportImportActivity.this, "json parse error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
