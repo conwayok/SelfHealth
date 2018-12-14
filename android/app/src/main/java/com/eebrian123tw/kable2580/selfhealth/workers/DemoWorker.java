9b3c25ccf89bfd86c62459d86e469638332c76cc
CONWAY <kable2580@gmail.com>
2018-12-14 14:18:22 +0800

    add work manager lib

---
+++ android/app/src/main/java/com/eebrian123tw/kable2580/selfhealth/workers/DemoWorker.java
@@ -0,0 +1,20 @@
+package com.eebrian123tw.kable2580.selfhealth.workers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class DemoWorker extends Worker {
+  public DemoWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+    super(context, workerParams);
+  }
+
+  @NonNull
+  @Override
+  public Result doWork() {
+
+    return null;
+  }
+}
